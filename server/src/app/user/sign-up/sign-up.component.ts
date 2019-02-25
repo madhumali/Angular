200612b4ceead38d0030122ec37ddb4fc2f1c2ed
madhumali <madhumalijanani@gmail.com>
2019-02-25 14:51:47 +0530

    register

---
+++ server/src/app/user/sign-up/sign-up.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import{NgForm} from '@angular/forms';
+
+import { UserService } from '../../shared/user.service'
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css'],
+  providers: [UserService]
+})
+export class SignUpComponent implements OnInit {
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  showSuccessMessage:boolean;
+  serverErrorMessages:string;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    this.userService.postUser(form.value).subscribe(
+      res => {
+        this.showSuccessMessage=true;
+        setTimeout(() =>this.showSuccessMessage=false,4000);
+        this.resetForm(form);
+      },
+      err => {
+        if(err.status === 422){
+          this.serverErrorMessages=err.error.join('<br/>');
+        }
+        else{
+          this.serverErrorMessages='Something went wrong,Please contact admin.';
+        }
+      }
+    );
+
+    }
+
+  resetForm(form:NgForm) {
+    this.userService.selectedUser = {
+      fullName:'',
+      email:'',
+      password:''
+  };
+    form.resetForm();
+    this.serverErrorMessages='';
+  }
+}
